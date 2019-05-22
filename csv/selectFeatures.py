b13bce4a7cb5f9c4e29e0aae576f9f37e864bd38
evoss98 <43595856+evoss98@users.noreply.github.com>
2019-05-22 16:49:45 -0700

    Update selectFeatures.py

--- csv/selectFeatures.py
+++ csv/selectFeatures.py
@@ -1,1 +1,17 @@
+import csv
+from sklearn import svm
+import pandas
 
+colnames = ['PTS_x', 'DRPG_x', 'TOPG_x', 'PF_x', 'RPG_x', 'Position', 'Percentile']
+data = pandas.read_csv('NCAA_NBA_merged.csv', names=colnames)
+
+
+
+
+
+
+
+  
+  
+ 
+  
