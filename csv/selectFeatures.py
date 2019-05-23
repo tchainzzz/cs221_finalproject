6ef393dea9a629e1d0ec919f0b9d4adfd548803d
evoss98 <43595856+evoss98@users.noreply.github.com>
2019-05-22 17:31:01 -0700

    Update selectFeatures.py

--- csv/selectFeatures.py
+++ csv/selectFeatures.py
@@ -1,90 +1,28 @@
 import csv
 from sklearn import svm
 import pandas
+from sklearn import datasets 
+from sklearn.metrics import confusion_matrix 
+from sklearn.model_selection import train_test_split 
 
-colnames = ['Player','PTS_x', 'DRPG_x', 'TOPG_x', 'PF_x', 'RPG_x', 'Position', 'Percentile']
-data = pandas.read_csv('final_data.csv', names=colnames)
+colnamesX = ['Player','PTS_x', 'DRPG_x', 'TOPG_x', 'PF_x', 'RPG_x', 'Position', 'Percentile']
+colnamesY = ['Class']
+dataX = pandas.read_csv('final_data.csv', names=colnamesX)
+dataY = pandas.read_csv('final_data.csv', names=colnamesY)
 
-import numpy as np
-import matplotlib.pyplot as plt
-
-from sklearn.datasets import make_multilabel_classification
-from sklearn.multiclass import OneVsOneClassifier
-from sklearn.svm import SVC
-from sklearn.decomposition import PCA
-from sklearn.cross_decomposition import CCA
-
-
-def plot_hyperplane(clf, min_x, max_x, linestyle, label):
-    # get the separating hyperplane
-    w = clf.coef_[0]
-    a = -w[0] / w[1]
-    xx = np.linspace(min_x - 5, max_x + 5)  # make sure the line is long enough
-    yy = a * xx - (clf.intercept_[0]) / w[1]
-    plt.plot(xx, yy, linestyle, label=label)
-
-
-def plot_subfigure(X, Y, subplot, title, transform):
-    if transform == "pca":
-        X = PCA(n_components=2).fit_transform(X)
-    elif transform == "cca":
-        X = CCA(n_components=2).fit(X, Y).transform(X)
-    else:
-        raise ValueError
-
-    min_x = np.min(X[:, 0])
-    max_x = np.max(X[:, 0])
-
-    min_y = np.min(X[:, 1])
-    max_y = np.max(X[:, 1])
-
-    classif = OneVsOneClassifier(SVC(kernel='linear'))
-    classif.fit(X, Y)
-
-    plt.subplot(2, 2, subplot)
-    plt.title(title)
-
-    zero_class = np.where(Y[:, 0])
-    one_class = np.where(Y[:, 1])
-    plt.scatter(X[:, 0], X[:, 1], s=40, c='gray', edgecolors=(0, 0, 0))
-    plt.scatter(X[zero_class, 0], X[zero_class, 1], s=160, edgecolors='b',
-                facecolors='none', linewidths=2, label='Class 1')
-    plt.scatter(X[one_class, 0], X[one_class, 1], s=80, edgecolors='orange',
-                facecolors='none', linewidths=2, label='Class 2')
-
-    plot_hyperplane(classif.estimators_[0], min_x, max_x, 'k--',
-                    'Boundary\nfor class 1')
-    plot_hyperplane(classif.estimators_[1], min_x, max_x, 'k-.',
-                    'Boundary\nfor class 2')
-    plt.xticks(())
-    plt.yticks(())
-
-    plt.xlim(min_x - .5 * max_x, max_x + .5 * max_x)
-    plt.ylim(min_y - .5 * max_y, max_y + .5 * max_y)
-    if subplot == 2:
-        plt.xlabel('First principal component')
-        plt.ylabel('Second principal component')
-        plt.legend(loc="upper left")
-
-
-plt.figure(figsize=(8, 6))
-
-X, Y = make_multilabel_classification(n_classes=10, n_labels=8,
-                                      allow_unlabeled=True,
-                                      random_state=1)
-
-plot_subfigure(X, Y, 1, "With unlabeled samples + CCA", "cca")
-plot_subfigure(X, Y, 2, "With unlabeled samples + PCA", "pca")
-
-X, Y = make_multilabel_classification(n_classes=10, n_labels=8,
-                                      allow_unlabeled=False,
-                                      random_state=1)
-
-plot_subfigure(X, Y, 3, "Without unlabeled samples + CCA", "cca")
-plot_subfigure(X, Y, 4, "Without unlabeled samples + PCA", "pca")
-
-plt.subplots_adjust(.04, .02, .97, .94, .09, .2)
-plt.show()
+# dividing X, y into train and test data 
+X_train, X_test, y_train, y_test = train_test_split(dataX, dataY, random_state = 0) 
+  
+# training a linear SVM classifier 
+from sklearn.svm import SVC 
+svm_model_linear = SVC(kernel = 'linear', C = 1).fit(X_train, y_train) 
+svm_predictions = svm_model_linear.predict(X_test) 
+  
+# model accuracy for X_test   
+accuracy = svm_model_linear.score(X_test, y_test) 
+  
+# creating a confusion matrix 
+cm = confusion_matrix(y_test, svm_predictions) 
 
 
 
