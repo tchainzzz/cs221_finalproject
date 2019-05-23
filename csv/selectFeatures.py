a89d2ce02d4fb64ba4cdaa5959dd66a0159fa666
evoss98 <43595856+evoss98@users.noreply.github.com>
2019-05-22 17:06:23 -0700

    Update selectFeatures.py

--- csv/selectFeatures.py
+++ csv/selectFeatures.py
@@ -69,14 +69,14 @@ def plot_subfigure(X, Y, subplot, title, transform):
 
 plt.figure(figsize=(8, 6))
 
-X, Y = make_multilabel_classification(n_classes=2, n_labels=1,
+X, Y = make_multilabel_classification(n_classes=10, n_labels=8,
                                       allow_unlabeled=True,
                                       random_state=1)
 
 plot_subfigure(X, Y, 1, "With unlabeled samples + CCA", "cca")
 plot_subfigure(X, Y, 2, "With unlabeled samples + PCA", "pca")
 
-X, Y = make_multilabel_classification(n_classes=2, n_labels=1,
+X, Y = make_multilabel_classification(n_classes=10, n_labels=8,
                                       allow_unlabeled=False,
                                       random_state=1)
 
